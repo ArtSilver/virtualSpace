d104e6c84c7c6bd5323470abd699895d6bc1563b
adriangeisow <developer@rede-silver.com>
2025-01-17 19:41:39 +0000

    debug text

--- sketch.js
+++ sketch.js
@@ -32,7 +32,8 @@ function draw() {
  
   imageMode(CENTER);
   image(bar, width*0.5, height-barH*barScale*0.5, barW*barScale, barH*barScale);
-  image(ears, earsX, earsY); 
+  image(ears, earsX, earsY);
+  text(`Ears at (${earsX}, ${earsY}), [${mouseX},${mouseY}]`, 10, 20);
   describe('the listener');
   //line(mouseX,mouseY, pmouseX, pmouseY);
 }
