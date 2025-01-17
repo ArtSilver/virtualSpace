3930196b04fe603a09a31f5b918fcd0a8c9fde88
adriangeisow <developer@rede-silver.com>
2025-01-17 16:25:02 +0000

    add Images

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let ears, bar,drums, bass;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 800);
 }
 
 function draw() {
   background(220);
+  imageMode(CORNER)
+  image(drums, 0, 0, 100, 100);
+  image(bass, width-100, 0, 100, 100);
+  image(bar, width/2-100, height-100, 100, 100);
+  imageMode(CENTER);
+  image(ears, width/2, height/2, 200, 200);
+}
+
+
+
+// Load the image and create a p5.Image object.
+function preload() {
+  ears = loadImage('/assets/ears.jpg');
+  bar = loadImage('/assets/bar.jpg');
+  drums = loadImage('/assets/drums.jpg');
+  bass = loadImage('/assets/bass.jpg');
 }
